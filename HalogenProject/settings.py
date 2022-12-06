90a8f18a8820f5f54233489a611d74f8753e514d
yongzee <ibress1234@gmail.com>
2022-12-06 17:07:53 +0100

    laaa

--- HalogenProject/settings.py
+++ HalogenProject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,6 +123,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+django_heroku.settings(locals())
 
 
 
